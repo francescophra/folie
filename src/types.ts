9ca1f251bf365fd16a1336b0bfff2b339a02e3bd
Giulio Bracci <109853281+giuliobracci@users.noreply.github.com>
2026-03-16 19:16:01 +0100

    feat(types): improve types accuracy
    BREAKING-CHANGE: this narrows the sets of approved options. Hence
    technically it's a breaking of the current contract.

--- src/types.ts
+++ src/types.ts
@@ -1,20 +1,57 @@
 export interface BreakpointConfig {
   columns: number
-  gutter: string
-  margin: string
+  gutter: CSSUnit
+  margin: CSSUnit
   until?: number
 }
 
 export interface FolieOptions {
   columns?: number
-  gutter?: string
-  margin?: string
+  gutter?: CSSUnit
+  margin?: CSSUnit
   breakpoints?: Record<string, BreakpointConfig>
-  color?: string
-  opacity?: number
+  color?: Color
+  opacity?: Opacity
   zIndex?: number
   shortcut?: string
   showOnStart?: boolean
   toggleButton?: boolean
   mode?: 'fill' | 'outline'
 }
+
+type CSSUnit =
+  | `${number}px` | `${number}pt` | `${number}pc`
+  | `${number}cm` | `${number}mm` | `${number}in`
+  | `${number}em` | `${number}rem` | `${number}ch` | `${number}ex`
+  | `${number}vw` | `${number}vh` | `${number}vmin` | `${number}vmax`
+  | `${number}svw` | `${number}svh`
+  | `${number}lvw` | `${number}lvh`
+  | `${number}dvw` | `${number}dvh`
+  | `${number}fr`
+  | '0'
+
+type Opacity = '0.1' | '0.2' | '0.3' | '0.4' | '0.5' | '0.6' | '0.7' | '0.8' | '0.9' | '1.0'
+
+type RGB = `rgb(${string})`
+type RGBA = `rgba(${string})`
+type HEX = `#${string}`
+type HSL = `hsl(${string})`
+type HSLA = `hsla(${string})`
+type Var = `var(${string})`
+type CustomProperty = `--${string}`
+type Transparent = 'transparent'
+type CurrentColor = 'currentColor'
+
+type CssGlobals = 'inherit' | 'initial' | 'revert' | 'unset'
+
+export type Color =
+  | CurrentColor
+  | Transparent
+  | RGB
+  | RGBA
+  | HEX
+  | HSL
+  | HSLA
+  | Var
+  | CustomProperty
+  | CssGlobals
